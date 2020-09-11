4054b4400ade55a56e25f7bc6fc5bcd4227cc9f9
Vladislav Ladan <vladislav.ladan.21@gmail.com>
2020-09-12 00:33:43 +0200

    add avoiding recursive logins functionality

--- src/main/java/dev/dagger/atm/configuration/module/LoginCommandModule.java
+++ src/main/java/dev/dagger/atm/configuration/module/LoginCommandModule.java
@@ -1,9 +1,11 @@
 package dev.dagger.atm.configuration.module;
 
 import dagger.Binds;
+import dagger.BindsOptionalOf;
 import dagger.Module;
 import dagger.multibindings.IntoMap;
 import dagger.multibindings.StringKey;
+import dev.dagger.atm.service.Database;
 import dev.dagger.atm.service.command.Command;
 import dev.dagger.atm.service.command.impl.LoginCommand;
 
@@ -14,4 +16,7 @@ public abstract class LoginCommandModule {
     @IntoMap
     @StringKey("login")
     public abstract Command loginCommand(LoginCommand loginCommand);
+
+    @BindsOptionalOf
+    public abstract Database.Account optionalAccount();
 }
--- src/main/java/dev/dagger/atm/service/command/impl/LoginCommand.java
+++ src/main/java/dev/dagger/atm/service/command/impl/LoginCommand.java
@@ -7,22 +7,32 @@ import dev.dagger.atm.service.Outputter;
 import dev.dagger.atm.service.command.SingleArgCommand;
 
 import javax.inject.Inject;
+import java.util.Optional;
 
 public final class LoginCommand extends SingleArgCommand {
 
     private final Outputter outputter;
     private final Database database;
     private final UserCommandsRouter.Factory userCommandsRouterFactory;
+    private final Optional<Database.Account> account;
 
     @Inject
-    public LoginCommand(Database database, Outputter outputter, UserCommandsRouter.Factory userCommandsRouterFactory) {
+    public LoginCommand(Database database,
+                        Outputter outputter,
+                        UserCommandsRouter.Factory userCommandsRouterFactory,
+                        Optional<Database.Account> account) {
         this.database = database;
         this.outputter = outputter;
         this.userCommandsRouterFactory = userCommandsRouterFactory;
+        this.account = account;
     }
 
     @Override
     protected Result handleArg(String username) {
+        if (account.isPresent()) {
+            // Ignore "login <foo>" commands if we already have an account
+            return Result.handled();
+        }
         Account account = database.getAccount(username);
         outputter.output(String.format("%s is logged in with balance: %s", username, account.balance()));
         return Result.enterNestedCommandSet(userCommandsRouterFactory.create(account).router());
