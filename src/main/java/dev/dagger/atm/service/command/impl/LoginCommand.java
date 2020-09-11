f972d2d0eaf314fa5f077ebc8c2cc1f97a478fa0
Vladislav Ladan <vladislav.ladan.21@gmail.com>
2020-09-11 13:16:23 +0200

    adjust LoginCommand service to use UserCommandsRouter

--- src/main/java/dev/dagger/atm/service/command/impl/LoginCommand.java
+++ src/main/java/dev/dagger/atm/service/command/impl/LoginCommand.java
@@ -1,5 +1,6 @@
 package dev.dagger.atm.service.command.impl;
 
+import dev.dagger.atm.configuration.factory.UserCommandsRouter;
 import dev.dagger.atm.service.Database;
 import dev.dagger.atm.service.Database.Account;
 import dev.dagger.atm.service.Outputter;
@@ -11,17 +12,19 @@ public final class LoginCommand extends SingleArgCommand {
 
     private final Outputter outputter;
     private final Database database;
+    private final UserCommandsRouter.Factory userCommandsRouterFactory;
 
     @Inject
-    public LoginCommand(Database database, Outputter outputter) {
+    public LoginCommand(Database database, Outputter outputter, UserCommandsRouter.Factory userCommandsRouterFactory) {
         this.database = database;
         this.outputter = outputter;
+        this.userCommandsRouterFactory = userCommandsRouterFactory;
     }
 
     @Override
     protected Result handleArg(String username) {
         Account account = database.getAccount(username);
-        outputter.output(String.format("%s is logged in with balance %s", username, account.balance()));
-        return Result.handled();
+        outputter.output(String.format("%s is logged in with balance: %s", username, account.balance()));
+        return Result.enterNestedCommandSet(userCommandsRouterFactory.create(account).router());
     }
 }
